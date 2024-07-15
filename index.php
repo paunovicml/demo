e8c5b13f82c5d2f75a851ca28806922ccb4caa16
paunovicml <mpaunovic@gmail.com>
2024-07-15 23:26:32 +0200

    Render the Notes and Note Pages

--- index.php
+++ index.php
@@ -2,14 +2,4 @@
 
 require 'functions.php';
 require 'Database.php';
-//require 'router.php';
-
-$config = require 'config.php';
-
-$db = new Database($config['database']);
-
-$notes = $db -> query("select * from notes") -> fetchAll();
-
-foreach ($notes as $note) {
-    echo "<li>" . $note['body'] . "</li>";
-}
+require 'router.php';
